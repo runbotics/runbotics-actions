b7f16f8e2e24b57b99d2e56694573fbed04dad25
Depcik <marcin.depcik@all-for-one.com>
2024-02-22 14:40:24 +0100

    feat(rpaframework): :sparkles: Add new actions

--- rpaframework-actions/main.py
+++ rpaframework-actions/main.py
@@ -1,34 +1,148 @@
 import argparse
-from rpaframework.actions import is_dialog_open
-from RPA.Desktop.Windows import Windows
+import json
+import rpaframework.actions as actions
+import sys
 
-win = Windows()
+APP_VERSION = "1.0.0"
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 
 def setup_args():
-    parser = argparse.ArgumentParser(description="List of options for RPA Framework actions")
+    parser = argparse.ArgumentParser(description='List of options for RPA Framework actions')
 
-    group1 = parser.add_argument_group('Option 1')
-    group2 = parser.add_argument_group('Option 2')
+    parser.add_argument('-windowTitle', '--windowTitle', required=True, help='Title of the app dialog')
 
-    group1.add_argument("--isDialogOpen", nargs=1, metavar=('title'), help="Title of the dialog to check if it is open")
-    group2.add_argument("--testOption", nargs=1, metavar=('message'), help="Message to print")
+    group1 = parser.add_argument_group('Get Element')
+    group1.add_argument('-getElement', '--getElementLocator', help='Get Element locator')
+    group2 = parser.add_argument_group('Get Window Elements')
+    group2.add_argument('-getWindowElements', '--getWindowElements', nargs='?', const=True, help='Get Window Elements parameters in JSON format')
+    group3 = parser.add_argument_group('Is Window Open')
+    group3.add_argument('-isWindowOpen', '--isWindowOpen', nargs='?', const=True, help='Check if window is open')
+    group4 = parser.add_argument_group('Mouse Click')
+    group4.add_argument('-mouseClick', '--mouseClickJsonBody', help='Mouse Click parameters in JSON format')
+    group5 = parser.add_argument_group('Wait For Element')
+    group5.add_argument('-waitForElement', '--waitForElementJsonBody', help='Wait For Element parameters in JSON format')
+    group6 = parser.add_argument_group('Press Keys')
+    group6.add_argument('-pressKeys', '--pressKeysJsonArray', help='Press keys in JSON Array format')
+    group7 = parser.add_argument_group('Send Keys')
+    group7.add_argument('-sendKeys', '--sendKeysJsonBody', help='Send keys parameters in JSON format')
+    group8 = parser.add_argument_group('Minimize Window')
+    group8.add_argument('-minimizeWindow', '--minimizeWindowLocator', help='Minimize window locator')
+    group9 = parser.add_argument_group('Maximize Window')
+    group9.add_argument('-maximizeWindow', '--maximizeWindowLocator', help='Maximize window locator')
+    group10 = parser.add_argument_group('Close Window')
+    group10.add_argument('-closeWindow', '--closeWindowLocator', help='Close window locator')
 
     return parser.parse_args()
 
 def action_handler():
     args = setup_args()
 
-    if args.isDialogOpen:
-        title = args.isDialogOpen[0]
+    window_title = args.windowTitle
 
-        print("Handling --isDialogOpen...")
-        is_dialog_open(title)
+    if args.isWindowOpen:
+        is_open = actions.is_window_open(window_title)
+        print(json.dumps(is_open))
+        return
 
-    if args.testOption:
-        message = args.testOption[0]
+    pid = actions.get_pid_by_window_title(window_title)
+    if pid is None: raise ValueError(f"Cannot get pid for window title '{window_title}'")
 
-        print("Handling --testOption...")
-        print("This is test message: ", message)
+    app = actions.connect_by_pid(pid)
+    if app is None: raise ValueError(f"Cannot connect to app by pid '{pid}'")
 
-if __name__ == "__main__":
+    if args.getElementLocator:
+        locator = args.getElementLocator
+
+        element = actions.get_element(locator)
+        if element is False: raise ValueError(f"Cannot find element for locator '{locator}'")
+
+        element_dict = vars(element)
+        element_dict.pop('item')
+        element_dict.pop('_sibling_element_compare')
+
+        print(json.dumps(element_dict))
+
+    if args.getWindowElements:
+        elements = actions.get_window_elements()
+        if not elements: raise ValueError(f"Cannot find window elements")
+
+        for element in elements:
+            element.pop('object')
+
+        print(json.dumps(elements))
+
+    if args.mouseClickJsonBody:
+        mouse_click_json = json.loads(args.mouseClickJsonBody)
+
+        locator = mouse_click_json.get('locator')
+        x = mouse_click_json.get('x')
+        y = mouse_click_json.get('y')
+
+        actions.mouse_click(locator, x, y)
+
+    if args.waitForElementJsonBody:
+        wait_for_element_json = json.loads(args.waitForElementJsonBody)
+
+        locator = wait_for_element_json.get('locator')
+        use_refreshing = wait_for_element_json.get('use_refreshing')
+        search_criteria= wait_for_element_json.get('search_criteria')
+        timeout= wait_for_element_json.get('timeout')
+        interval= wait_for_element_json.get('interval')
+
+        elements = actions.wait_for_element(
+            locator,
+            use_refreshing,
+            search_criteria,
+            timeout,
+            interval,
+        )
+        if not elements: raise ValueError(f"Cannot find elements for locator '{locator}'")
+
+        for element in elements:
+            element.pop('object')
+            element.pop('control')
+
+        print(json.dumps(elements))
+
+
+    if args.pressKeysJsonArray:
+        press_keys = json.loads(args.pressKeysJsonArray)
+
+        actions.press_keys(press_keys)
+
+    if args.sendKeysJsonBody:
+        send_keys_json = json.loads(args.sendKeysJsonBody)
+
+        locator = send_keys_json.get('locator')
+        keys = send_keys_json.get('keys')
+        interval = send_keys_json.get('interval')
+        wait_time = send_keys_json.get('wait_time')
+        send_enter = send_keys_json.get('send_enter')
+
+        actions.send_keys(
+            locator,
+            keys,
+            interval,
+            wait_time,
+            send_enter
+        )
+
+    if args.minimizeWindowLocator:
+        locator = args.minimizeWindowLocator
+
+        actions.minimize_window(locator)
+
+    if args.maximizeWindowLocator:
+        locator = args.maximizeWindowLocator
+
+        actions.maximize_window(locator)
+
+    if args.closeWindowLocator:
+        locator = args.closeWindowLocator
+
+        actions.close_window(locator)
+
+if __name__ == '__main__':
     action_handler()
--- rpaframework-actions/rpaframework/actions.py
+++ rpaframework-actions/rpaframework/actions.py
@@ -1,15 +1,97 @@
-from RPA.Desktop.Windows import Windows
+import RPA.Desktop.Windows as desktop_windows
+import RPA.Windows as windows
+import RPA.Desktop as desktop
 
-win = Windows()
+desk_win = desktop_windows.Windows()
+win = windows.Windows()
+desk = desktop.Desktop()
 
-def is_dialog_open(title: str):
-    windows = win.get_window_list()
-
-    is_dialog_open = False
+def is_window_open(title: str):
+    windows = win.list_windows()
+    is_open = False
 
     for window in windows:
         window_title = window.get('title')
         if (window_title == title):
-            is_dialog_open = True
+            is_open = True
 
-    print(is_dialog_open)
+    return is_open
+
+def get_pid_by_window_title(title: str):
+    windows = win.list_windows()
+
+    for window in windows:
+        window_title = window.get('title')
+        if (window_title == title):
+            return int(window.get('pid'))
+
+    return None
+
+def connect_by_pid(app_pid: str, window_title: str = None):
+    return desk_win.connect_by_pid(app_pid, window_title)
+
+def get_element(locator: str):
+    return win.get_element(locator)
+
+def get_window_elements():
+    return win.list_windows()
+
+def mouse_click(
+    locator: str = None,
+    x: int = 0,
+    y: int = 0,
+):
+    if x is None: x = 0
+    if y is None: y = 0
+
+    desk_win.mouse_click(locator, x, y)
+
+def wait_for_element(
+    locator: str,
+    use_refreshing: bool = False,
+    search_criteria: str = None,
+    timeout: float = 30.0,
+    interval: float = 2.0,
+):
+    if use_refreshing is None: use_refreshing = False
+    if timeout is None: timeout = 30.0
+    if interval is None: interval = 2.0
+
+    return desk_win.wait_for_element(
+        locator,
+        use_refreshing,
+        search_criteria,
+        timeout,
+        interval
+    )
+
+def press_keys(keys):
+    keys_to_press = [key for key in keys if key is not None]
+    desk.press_keys(*keys_to_press)
+
+def send_keys(
+    locator: str | None,
+    keys: str | None,
+    interval: float | None,
+    wait_time: float | None,
+    send_enter: bool | None
+):
+    if interval is None: interval = 0.1
+    if send_enter is None: send_enter = False
+
+    win.send_keys(
+        locator,
+        keys,
+        interval,
+        wait_time,
+        send_enter
+    )
+
+def minimize_window(locator: str):
+    return win.minimize_window(locator)
+
+def maximize_window(locator: str):
+    return win.maximize_window(locator)
+
+def close_window(locator: str):
+    return win.close_window(locator)
